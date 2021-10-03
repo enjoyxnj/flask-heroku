041808e7fd0f6ac35e9fee956ecf99b9741ae4c4
enjoyxnj <89239189+enjoyxnj@users.noreply.github.com>
2021-10-03 23:23:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok" 
+    return "Hello สุธาสินี ไชยโพธิ์ เลขที่20 ชั้น ม.4/7" 
 
 @app.route('/api', methods=['GET'])
 def get_api():
